0684e43f8f8e8e2d50d1b932906ad1921b61f931
rajiv-fullstack <rajivchoudhary278@gmail.com>
2023-08-24 12:20:38 +0530

    first push

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
+ 
+axios.interceptors.request.use(function (config) {
+  config.headers['binarybox_api_key_VzO8M31mfzUAW58MBuDkyVX68IWufWJWW7m5BqqSi3FSXHHwKeHjuXQzOC7QdACzffplQ93npFb6Q3sMQLeImXxkz3IHQDkWy1yt'] = process.env.REACT_APP_API_KEY;
+  return config;
+});
+ 
+ 
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
+ 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
